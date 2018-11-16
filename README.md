24e1ddc67806d134e0293bf197b8597305a35163
Unknown <alextehturtle@gmail.com>
2018-11-15 23:19:33 -0600

    Added both play and skip feature
    used "Music bot from scratch in node.js" video

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Music bot named 0w0
-This is a discord bot project I am working on, for now he will be a music bot but will be adding more features in the future.
+# Music client named 0w0
+This is a discord client project I am working on, for now he will be a music client but will be adding more features in the future.
--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const opusscript = require("opusscript");
 const ytdl = require("ytdl-core");
 const fs = require("fs");
 const pack = require("./package.json");
+const getYoutubeID = require("get-youtube-id");
+const fetchVideoInfo = require("youtube-info");
+
+var queue = [];
+var isPlaying = false;
+var dispatcher = null;
+var voiceChannel = null;
+var skipReq = 0;
+var skippers = [];
 
 function resetBot(channel) {
 	channel.send({embed: {
@@ -38,12 +47,16 @@ function shutdown(channel) {
 	bot.destroy();
 }
 
+
+
 bot.on("ready", function() {
 	bot.user.setActivity("with DJ Set");
 	console.log("I am weady UwU!");
 });
 
-bot.on("message", async function(msg, args, ops) {
+bot.on("message", function(msg) {
+	var voicechannel = msg.member.voiceChannel;
+
 	switch(msg.content) {
 		// Basic 0w0 commands
 		case config.prefix + "ping":
@@ -95,31 +108,154 @@ bot.on("message", async function(msg, args, ops) {
 				break;
 			//Summon and leave command for 0w0
 			case config.prefix + "leave":
-                msg.member.voiceChannel.leave();
+                voicechannel.leave();
                 msg.reply("Don't have to be so wude I wiww weave UmU");
                 break;
             case config.prefix + "summon":
-                msg.member.voiceChannel.join();
-                msg.reply("I've been summoned!!! UwU");
+				if(msg.member.voiceChannel) {
+					if(!msg.guild.voiceConnection) {
+						voicechannel.join()
+							.then(connection => {
+								msg.reply("I've been summoned!!! UwU");
+							});
+					}
+				} else {
+					msg.reply("You must be in a voice channew UmU!!!");
+				}
 				break;
-			//Music commands for 0w0
-			case config.prefix + "play":
-                const ytdl = require('ytdl-core');
-                const streamOptions = { seek: 0, volume: 1 };
-                const broadcast = bot.createVoiceBroadcast();
-                
-                //play(connection, msg);
-                msg.member.voiceChannel.join().then(connection => {
-                    const stream = ytdl('https://youtu.be/UzZGO9hgpBU', { filter : 'audioonly' });
-                    broadcast.playStream(stream);
-                    const dispatcher = connection.playBroadcast(broadcast);
-                })
-                .catch(console.error);
+			// Old music command i need to test out again
+			/*case config.prefix + "play":
+				const ytdl = require('ytdl-core');
+				const streamOptions = { seek: 0, volume: 1 };
+				const broadcast = bot.createVoiceBroadcast();
+				if (!msg.content.startsWith(config.prefix) || msg.author.bot) return;
+				const args = msg.content.slice(config.prefix.length).split(' ');
+				const command = args.shift().toLowerCase();
+					
+				//play(connection, msg);
+				voiceChannel.join().then(connection => {
+					const stream = ytdl(args[0], { filter : 'audioonly' });
+					broadcast.playStream(stream);
+					const dispatcher = connection.playBroadcast(broadcast);
+            	})
+            	.catch(console.error);
+				break;*/
+			// Do not use this play command above
+
+			case config.prefix + "play": 
+
+				break;
+			case config.prefix + "skip":
+
 				break;
 			}
 		}
 );
-	
 
+
+
+function getID(str, cb) {
+	if(isYoutube(str)) {
+		cb(getYoutubeID(str));
+	} else {
+		search_video(str, function(id) {
+			cb(id);
+		});
+	}
+}
+
+function add_to_queue(strID) {
+	if(isYoutube(strID)) {
+		queue.push(getYoutubeID(strID));
+	} else {
+		queue.push(strID);
+	}
+}
+
+function search_video(query, callback) {
+    request("https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=" + encodeURIComponent(query) + "&key=" + config.ytKey, function(error, response, body) {
+        var json = JSON.parse(body);
+        if (!json.items[0]) callback("3_-a9nVZYjk");
+        else {
+            callback(json.items[0].id.videoId);
+        }
+    });
+}
+
+function isYoutube(str) {
+	return str.toLowerCase().indexOf("youtube.com") > -1;
+}
+
+bot.on("message", function(msg, args) {
+
+	const member = msg.member;
+	const mess = msg.content.toLowerCase();
+	var args = msg.content.split(" ").slice(1).join(" ");
+
+	if(mess.startsWith(config.prefix + "play"))  {
+		if(member.voiceChannel || bot.guilds.get("409417571574480896").voiceConnection != null) {
+			if(queue.length > 0 || isPlaying) {
+				getID(args, function(id) {
+					add_to_queue(id);
+					fetchVideoInfo(id, function(err, videoInfo) {
+						if(err) throw new Error(err);
+						msg.reply(" Added to queue UwU: **" + videoInfo.title + "**");
+					});
+				});
+			} else {
+				isPlaying = true;
+				getID(args, function(id) {
+					queue.push("placeholder");
+					playMusic(id, msg);
+					fetchVideoInfo(id, function(err, videoInfo) {
+						if(err) throw new Error(err);
+						msg.reply(" Now pwaying UwU: **" + videoInfo.title + "**");
+					});
+				});
+		}
+	} else {
+		msg.reply(" You need to be in voice channew UmU!!!");
+	}
+	} else if (mess.startsWith(config.prefix + "skip")) {
+		if(skippers.indexOf(msg.author.id) === -1) {
+			skippers.push(msg.author.id);
+			skipReq++;
+			if(skipReq >= Math.ceil((voiceChannel.members.size - 1) / 2)) {
+				skipSong(msg);
+				msg.reply(" Youw skip was acknowedged UwU!!!");
+			} else {
+				msg.reply(" You need **" + Math.ceil((voiceChannel.members.size - 1) / 2) - skipReq) + "** mowe skip votes UmU";
+			}
+		} else {
+			msg.reply(" You aweady voted to skip!!! UwU");
+		}
+	}
+});
+
+function skipSong (msg) {
+	dispatcher.end();
+}
+
+function playMusic(id, msg) {
+	voiceChannel = msg.member.voiceChannel;
+	voiceChannel.join().then(function(connection) {
+		stream = ytdl("https://www.youtube.com/watch?v=" + id, { filter: "audioonly"});
+		skipReq = 0;
+		skippers = [];
+
+		dispatcher = connection.playStream(stream);
+		dispatcher.on("end", function() {
+			skipReq = 0;
+			skippers = [];
+			queue.shift();
+			if(queue.length === 0) {
+				queue = [];
+				isPlaying = false;
+			} else {
+				playMusic(queue[0], msg);
+			}
+		});
+	});
+}
 
 bot.login(config.token);
